c32224c686c91774146c9676a9de47094eb28e64
BNSebastian <bucovala.sebastian@protonmail.com>
2024-12-04 08:49:06 +0200

    updated some settings

---
+++ api/Models/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace api.Models.Entities
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public required byte[] PasswordHash { get; set; }
+
+        [Required]
+        public required byte[] PasswordSalt { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Username must be between 3 and 100 characters.")]
+        public required string UserName { get; set; }
+    }
+}
