481101c8870c041a4977c85e6bdde69975ffed9e
BNSebastian <bucovala.sebastian@protonmail.com>
2024-12-06 21:39:59 +0200

    update

---
+++ api/Tests/Users.ps1
@@ -0,0 +1,22 @@
+$apiUri = "http://localhost:5271/api/User"
+$currentDirectory = Get-Location
+
+$ALERT = "$currentDirectory\Methods\Alert.ps1"
+$GET = "$currentDirectory\Methods\Get.ps1"
+$POST = "$currentDirectory\Methods\Post.ps1"
+
+$bearerToken = ""
+
+$newUser = @{
+    userName = "wtf"
+}
+
+$createMsg = . $ALERT -Message "Creating a new user"
+Write-Output $newUser = . $POST -EndpointUri $apiUri -Message $createMsg -BearerToken $bearerToken -Body $newUser
+
+$getByIdMsg = . $ALERT -Message "Getting user by id"
+$id = $newUser.$id
+. $GET -EndpointUri "$apiUri/$id" -Message $getByIdMsg -BearerToken $bearerTokenj
+
+$getAllMsg = . $ALERT -Message "Getting all users"
+. $GET -EndpointUri $apiUri -Message $getAllMsg -BearerToken $bearerTokenj
